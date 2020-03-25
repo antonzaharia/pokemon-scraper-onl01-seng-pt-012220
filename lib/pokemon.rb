b2be49c75afaaa3e71de23f41a73ccd174722f0e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-25 17:08:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,15 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
   
+  @@all = []
+  
   def initialize(id, name, type, db, hp)
     @id = id
     @name = name
     @type = type
     @db = db
     @hp = hp
+    
+    @@all << self
   end
 end
